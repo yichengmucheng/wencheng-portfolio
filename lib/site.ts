7b263acfadd59278d3cee85df2625a7c022693a0
yichengmucheng <1099288401@qq.com>
2026-09-20 15:32:17 +0800

    chore: use muchenglab.com as canonical site URL

--- lib/site.ts
+++ lib/site.ts
@@ -1,1 +1,1 @@
-export const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000").replace(/\/$/, "");
+export const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "https://muchenglab.com").replace(/\/$/, "");
